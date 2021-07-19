1e6394f1eb6aca0f4d55c169fabcf55e3035db35
Sanobe1 <valentenkos@gmail.com>
2021-07-19 17:57:58 +0300

    4534534

---
+++ app/src/main/java/com/game/gmm/PolicyActivity.java
@@ -0,0 +1,13 @@
+package com.game.gmm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policy);
+    }
+}
